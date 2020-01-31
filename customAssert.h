a9dbc8c473aba59f3c1a04aa504dddff42d47c7a
Jacotsu <Jacotsu@users.noreply.github.com>
2020-01-31 22:32:19 +0100

    Improved Asserts logging

--- customAssert.h
+++ customAssert.h
@@ -4,12 +4,19 @@
 #include "slog.h"
 
 #ifdef DEBUG
+    // 0    panic
+    // 1    fatal
+    // 2    error
+    // 3    warn
+    // 4    info
+    // 5    live
+    // 6    debug
     #define Assert(x)  if((x) == 0){\
-        slog_fatal(0, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
+        slog_fatal(1, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
         exit(1);}
     #define LoggedAssert(x, ...)  if((x) == 0){\
-        slog_fatal(0, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
-        slog_fatal(0, __VA_ARGS__ );\
+        slog_fatal(1, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
+        slog_fatal(1, __VA_ARGS__ );\
         exit(1);}
 
 #else
