86bd8eaee369719ecc7e8ec22d93d9f95aeeb8d1
Jacotsu <Jacotsu@users.noreply.github.com>
2020-02-06 12:21:31 +0100

    Reduced custom assert verbosity

--- src/includes/customAssert.h
+++ src/includes/customAssert.h
@@ -11,10 +11,10 @@
 // 5    live
 // 6    debug
 #define Assert(x)  if((x) == 0){\
-    slog_fatal(1, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
+    slog_fatal(1, "Assertion failed: %s",  x);\
     exit(1);}
 #define LoggedAssert(x, ...)  if((x) == 0){\
-    slog_fatal(1, "%s:%d:Assertion failed: %s", __FILE__, __LINE__, x);\
+    slog_fatal(1, "Assertion failed: %s", x);\
     slog_fatal(1, __VA_ARGS__ );\
     exit(1);}
 
