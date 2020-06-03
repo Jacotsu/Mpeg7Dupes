c5c4d0df0170154efdc3c6ac6e3fd38029efec90
Jacotsu <Jacotsu@users.noreply.github.com>
2020-06-03 12:11:43 +0200

    Changed symbolic link to script to hardlink Fixed typo in script name

--- scripts/video_generate_signatures.sh
+++ scripts/video_generate_signatures.sh
@@ -1,1 +1,25 @@
-/home/jacotsu/scripts/video_generate_signatures.sh
+#!/usr/bin/env bash
+
+# Usage
+# ...: {input file 1}...{last input file} {signature dir}
+
+
+# The last element is the output directory
+OUTDIR="${@:$#}"
+
+mkdir -p "$OUTDIR"
+
+for file in "${@:1:$#-1}"; do
+    BASENAME=$(basename "$file")
+    SIGNATURE_FILE="$OUTDIR/$BASENAME.sig"
+
+    echo "Processing $BASENAME signature"
+
+    if [ ! -f "$SIGNATURE_FILE" ]; then
+        ffmpeg -i "$file" -hide_banner -filter_complex \
+            signature=detectmode=off:filename="$SIGNATURE_FILE":nb_inputs=1 \
+            -f null -
+
+        echo "Added $BASENAME signature"
+    fi
+done
